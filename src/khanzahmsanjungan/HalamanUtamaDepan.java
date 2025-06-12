2b0c8ad230a0b1ad3b6d785b3e216ce8bde9a4bc
Muhammad Rizky Rizaldi <myquinn489@gmail.com>
2025-06-12 09:04:57 +0800

    wip
    Signed-off-by: Muhammad Rizky Rizaldi <myquinn489@gmail.com>

--- src/khanzahmsanjungan/HalamanUtamaDepan.java
+++ src/khanzahmsanjungan/HalamanUtamaDepan.java
@@ -5,8 +5,6 @@
 package khanzahmsanjungan;
 
 import fungsi.koneksiDB;
-import fungsi.sekuel;
-import fungsi.validasi;
 import java.awt.Dimension;
 import static java.awt.Frame.MAXIMIZED_BOTH;
 import java.awt.Toolkit;
@@ -21,8 +19,8 @@ import javax.swing.JOptionPane;
 public class HalamanUtamaDepan extends javax.swing.JFrame {
 
     private static HalamanUtamaDepan myInstance;
-    private sekuel Sequel = new sekuel();
-    private validasi Valid = new validasi();
+    private final boolean AKTIFKANREGISTRASIBPJS = koneksiDB.AKTIFKANREGISTRASIBPJS(),
+                          AKTIFKANREGISTRASIEKSEKUTIF = koneksiDB.AKTIFKANREGISTRASIEKSEKUTIF();
 
     /**
      * Creates new form HalamanUtamaDepan
@@ -42,6 +40,8 @@ public class HalamanUtamaDepan extends javax.swing.JFrame {
         }
         
         jPanel1.repaint();
+        
+        pack();
     }
 
     /**
@@ -301,13 +301,7 @@ public class HalamanUtamaDepan extends javax.swing.JFrame {
                     break;
                 }
             }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(HalamanUtamaDepan.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(HalamanUtamaDepan.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(HalamanUtamaDepan.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | javax.swing.UnsupportedLookAndFeelException ex) {
             java.util.logging.Logger.getLogger(HalamanUtamaDepan.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
